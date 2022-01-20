75e8a5fd5e890ddf4e66d8cab7f73ff811f6eee2
leo <leonardo.gomes1@a.metodista.br>
2022-01-19 21:34:21 -0300

    fix: json dto return on home

--- src/main/java/br/com/kandone/controller/dto/HomeDTO.java
+++ src/main/java/br/com/kandone/controller/dto/HomeDTO.java
@@ -7,20 +7,49 @@ import java.util.stream.Collectors;
 import br.com.kandone.controller.form.CardFormHome;
 import br.com.kandone.model.Board;
 import br.com.kandone.model.Card;
+import br.com.kandone.model.statusCard;
 
 public class HomeDTO {
 	
 	private BoardDTO board;
-	private List<CardFormHome> cards;
 	
-	public List<CardFormHome> getCards() {
-		return cards;
+	private List<CardFormHome> cards_backlog;
+	private List<CardFormHome> cards_todo;
+	private List<CardFormHome> cards_inprogress;
+	private List<CardFormHome> cards_done;
+	
+
+	public List<CardFormHome> getCards_backlog() {
+		return cards_backlog;
 	}
-	
-	public void setCards(List<CardFormHome> cards) {
-		this.cards = cards;
+
+	public void setCards_backlog(List<CardFormHome> cards_backlog) {
+		this.cards_backlog = cards_backlog;
 	}
-	
+
+	public List<CardFormHome> getCards_inprogress() {
+		return cards_inprogress;
+	}
+
+	public void setCards_inprogress(List<CardFormHome> cards_inprogress) {
+		this.cards_inprogress = cards_inprogress;
+	}
+
+	public List<CardFormHome> getCards_done() {
+		return cards_done;
+	}
+
+	public void setCards_done(List<CardFormHome> cards_done) {
+		this.cards_done = cards_done;
+	}
+
+	public List<CardFormHome> getCards_todo() {
+		return cards_todo;
+	}
+
+	public void setCards_todo(List<CardFormHome> cards_todo) {
+		this.cards_todo = cards_todo;
+	}
 
 	public BoardDTO getBoard() {
 		return board;
@@ -32,8 +61,26 @@ public class HomeDTO {
 
 	public HomeDTO(List<Card> card, Board board) {
 		this.board = new BoardDTO(board);
-		this.cards = new ArrayList<>();
-		this.cards.addAll(card.stream().map(CardFormHome::new).collect(Collectors.toList()));
+		
+		this.cards_backlog = new ArrayList<>();
+		this.cards_backlog.addAll(card.stream()
+				.filter(c -> c.getStatus() == statusCard.BACKLOG)
+				.map(CardFormHome::new).collect(Collectors.toList()));
+		
+		this.cards_todo = new ArrayList<>();
+		this.cards_todo.addAll(card.stream()
+				.filter(c -> c.getStatus() == statusCard.TO_DO)
+				.map(CardFormHome::new).collect(Collectors.toList()));
+		
+		this.cards_inprogress = new ArrayList<>();
+		this.cards_inprogress.addAll(card.stream()
+				.filter(c -> c.getStatus() == statusCard.IN_PROGRESS)
+				.map(CardFormHome::new).collect(Collectors.toList()));
+		
+		this.cards_done = new ArrayList<>();
+		this.cards_done.addAll(card.stream()
+				.filter(c -> c.getStatus() == statusCard.DONE)
+				.map(CardFormHome::new).collect(Collectors.toList()));
 	}
 
 }
